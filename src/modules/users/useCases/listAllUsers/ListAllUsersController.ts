9e663f40c1f2dce51a4fc33693cd085b1a6afab8
João Reis <joaoreisjpk@gmail.com>
2022-02-20 12:02:31 -0300

    creating the ListAllUsersController

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,17 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers as { user_id: string };
+
+      const users = this.listAllUsersUseCase.execute({
+        user_id,
+      });
+
+      return response.status(200).json(users);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
